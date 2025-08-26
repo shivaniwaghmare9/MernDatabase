15300916983ef15706550f8754c6e29c3885705b
shivani <shivaniw69@gmail.com>
2025-08-26 18:06:28 +0530

    route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,16 +1,4 @@
 
-// const express=require("express")
-// const route=express.Router();
-// const userController=require("../controllers/userController")
-
-
-// route.post("/register",userController.userRegister);
-// route.post("/login",userController.userLogin);
-
-
-// module.exports=route;
-
-
 const express=require("express")
 const route=express.Router();
 const userController=require("../controllers/userController")
