f54da13b7aa022c8bcb44452ba38af101a833a2a
shivani <shivaniw69@gmail.com>
2025-08-29 22:16:14 +0530

    regi

--- frontend/src/Pages/Registration.jsx
+++ frontend/src/Pages/Registration.jsx
@@ -1,57 +1,4 @@
 
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
-// import { useState } from 'react';
-// import BackendUrl from '../Utils/BackendURL';
-// import axios from "axios";
-// import {ToastContainer,toast} from "react-toastify"
-
-// const Registration=()=>{
-//     const [input,setInput]=useState({});
-//     const handleInput=(e)=>{
-//         let name=e.target.name;
-//         let value=e.target.value;
-//         setInput(Values=>({...Values,[name]:value}))
-//         console.log(input)
-//     }
-//     const handleSubmit=async(e)=>{
-//         e.preventDefault();
-//         let api=`${BackendUrl}user/register`;
-//         const response=await axios.post(api,input);
-//         console.log(response.data);
-//       toast.success("data successfully saved")
-//     }
-//     return(
-//         <>
-
-//         <h3 className='h33'>Registration page!!!</h3>
-//           <Form id="form">
-//       <Form.Group className="mb-3" >
-//         <Form.Label>Name</Form.Label>
-//         <Form.Control type="text"  name="name" onChange={handleInput}/>
-//      </Form.Group>
-
-//       <Form.Group className="mb-3" >
-//         <Form.Label>Email</Form.Label>
-//         <Form.Control type="eamil"  name="email" onChange={handleInput}/>
-//      </Form.Group>
-
-//       <Form.Group className="mb-3" >
-//         <Form.Label>Password</Form.Label>
-//         <Form.Control type="password"  name="password" onChange={handleInput}/>
-//      </Form.Group>
-
-     
-//       <Button variant="primary" type="submit" onClick={handleSubmit}>
-//         Submit
-//       </Button>
-//     </Form>
-//     <ToastContainer/>
-//         </>
-//     )
-// }
-// export default Registration;
-
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
@@ -104,3 +51,4 @@ const Registration=()=>{
     )
 }
 export default Registration;
+
