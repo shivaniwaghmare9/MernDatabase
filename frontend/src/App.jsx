b07909b230fca420fe9f0736c1e27c3827f44a08
shivani <shivaniw69@gmail.com>
2025-08-29 22:16:53 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,28 @@
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Layout from "./Layout";
+// import Home from "./Pages/Home";
+// import Registration from "./Pages/Registration";
+// import Login from "./Pages/Login";
+
+
+// const App=()=>{
+//   return(
+//     <>
+//      <BrowserRouter>
+//          <Routes>
+//           <Route path="/" element={<Layout/>}>
+//           <Route index element={<Home/>}/>
+//           <Route path="home" element={<Home/>}/>
+//           <Route path="registration" element={<Registration/>}/>
+//           <Route path="login" element={<Login/>}/>
+          
+//           </Route>
+//          </Routes>
+//      </BrowserRouter>
+//     </>
+//   )
+// }
+// export default App;
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Pages/Home";
