07782c382b93f2fbea1aaac063d30753a7c7b0b4
shivani <shivaniw69@gmail.com>
2025-09-01 15:18:19 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,29 @@
 
+// const express=require("express");
+// const app=express();
+// const userRoute=require("./routes/userRoute")
+// const mongoose=require("mongoose");
+// const bodyparser=require("body-parser");
+// require("dotenv").config();
+// const cors=require("cors")
+// mongoose.connect(process.env.DBCON).then(()=>{
+//     console.log("Database Successfully Connected")
+// })
+
+// //Bodyparser middleware
+// app.use(bodyparser.urlencoded({extended:true}));
+// app.use(bodyparser.json());
+
+// app.use(cors());
+// app.use("/user",userRoute)
+
+
+// const port=process.env.PORT || 8000
+
+// app.listen(port,()=>{
+//     console.log(`Server is running on port :${port}`)
+// })
+
 const express=require("express");
 const app=express();
 const userRoute=require("./routes/userRoute")
