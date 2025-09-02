4c187d38c633ba81b58b768094d378d0e1c5698b
shivani <shivaniw69@gmail.com>
2025-09-02 23:42:05 +0530

    men

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,8 +1,25 @@
 
+import { useNavigate} from "react";
+import { useEffect } from "react";
 const Dashboard=()=>{
+    const navigate=useNavigate();
+    const logout=()=>{
+        localStorage.clear();
+        navigate("/login")
+    }
+    useEffect(()=>{
+        if(!localStorage.getItem("usename"))
+        {
+            navigate("/login")
+        }
+    })
     return(
         <>
-        
+         <h3>Welcome to user</h3>
+         <div>
+            Welcome :{localStorage.getItem("username")} Email:{localStorage.getItem("useremail")}
+            <a href="#" onClick={logout}>Logout</a>
+         </div>
         </>
     )
         
