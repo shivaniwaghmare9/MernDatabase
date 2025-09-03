4142fa421a344f4cd521ce1e4cc854599111ce09
shivani <shivaniw69@gmail.com>
2025-09-03 22:47:23 +0530

    dashbord

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,4 +1,32 @@
 
+// import { useNavigate} from "react";
+// import { useEffect } from "react";
+// const Dashboard=()=>{
+//     const navigate=useNavigate();
+//     const logout=()=>{
+//         localStorage.clear();
+//         navigate("/login")
+//     }
+//     useEffect(()=>{
+//         if(!localStorage.getItem("usename"))
+//         {
+//             navigate("/login")
+//         }
+//     })
+//     return(
+//         <>
+//          <h3>Welcome to user</h3>
+//          <div>
+//             Welcome :{localStorage.getItem("username")} Email:{localStorage.getItem("useremail")}
+//             <a href="#" onClick={logout}>Logout</a>
+//          </div>
+//         </>
+//     )
+        
+// }
+// export default Dashboard;
+
+
 import { useNavigate} from "react";
 import { useEffect } from "react";
 const Dashboard=()=>{
